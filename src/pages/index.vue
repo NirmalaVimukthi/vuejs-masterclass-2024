dcaf453c6e46777a59ed8664d8cd9ca4711fd4b7
NirmalaVimukthi <nirmalabandara96@gmail.com>
2024-11-02 19:01:20 +0530

    not found and pass multiple parameter with vue get router as props

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Home Page </h1>
+    <RouterLink to="/about" >About Page</RouterLink>
+    <RouterLink to="/page/1" >Page 1</RouterLink>
+    <RouterLink :to="{name:'/page/[id]', params:{id:108}}" >Page 108</RouterLink>
+    <RouterLink :to="{name:'/page/[id]', params:{id:'Nirmala'}}" >Page Nirmala</RouterLink>
+  </div>
+</template>
